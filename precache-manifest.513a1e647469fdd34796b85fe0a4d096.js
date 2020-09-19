082cf7881ef5514ed71fc8289a2d6e97df7d563d
Philane Msibi <Philane.msibi@dfafrica.co.za>
2020-09-19 10:03:03 +0200

    Updates

---
+++ precache-manifest.513a1e647469fdd34796b85fe0a4d096.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b835c5f265f913c7e89c336ef30095af",
+    "url": "/github-user-profile-app/index.html"
+  },
+  {
+    "revision": "570cc55a17e050d7537d",
+    "url": "/github-user-profile-app/static/css/main.13dbe0b1.chunk.css"
+  },
+  {
+    "revision": "9a1982c8fa438d55ef64",
+    "url": "/github-user-profile-app/static/js/2.a90bbd37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-user-profile-app/static/js/2.a90bbd37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "570cc55a17e050d7537d",
+    "url": "/github-user-profile-app/static/js/main.b9f6be2e.chunk.js"
+  },
+  {
+    "revision": "29178ef1292572c41fe6",
+    "url": "/github-user-profile-app/static/js/runtime-main.0ff46afb.js"
+  }
+]);
